4c084caa95aa1b08fc6b0a0863488b20d982a264
Joseperez <51133263+joseperu2503@users.noreply.github.com>
2023-08-22 21:43:49 -0500

    Update Input.vue

--- src/components/shared/Input.vue
+++ src/components/shared/Input.vue
@@ -1,33 +1,18 @@
 <template>
   <div class="relative">
-    <label class="block mb-0.5 text-sm font-medium text-rick-white">{{label}}</label>
+    <label class="block mb-0.5 text-sm font-medium text-rick-white">{{ label }}</label>
     <div class="flex relative items-center">
-      <input
-        :value="modelValue"
-        @input="input"
-        :type="type == 'password' ? showPassword ? 'text' : 'password'  : type"
-        class="h-10 bg-transparent outline-none text-rick-white text-sm block w-full p-2.5"
-        :class="[
-          error ? 'border-red-400 border-b focus:border-red-500' : 'border-b border-rick-green-1 focus:border-rick-green-2' ,
-          {'pr-10': type == 'password'}
-        ]"
-        :placeholder="placeholder"
-        :name="name"
-      />
-      <Icon
-       v-if="showPassword && type=='password'"
-        icon="mdi:eye-outline"
-        class="absolute right-2.5 text-rick-white cursor-pointer"
-        @click="togglePassword"
-      />
-      <Icon
-        v-if="!showPassword && type=='password'"
-        icon="mdi:eye-off-outline"
-        class="absolute right-2.5 text-rick-white cursor-pointer"
-        @click="togglePassword"
-      />
+      <input :value="modelValue" @input="input" :type="type == 'password' ? showPassword ? 'text' : 'password' : type"
+        class="h-10 bg-transparent outline-none text-rick-white text-sm block w-full p-2.5" :class="[
+          error ? 'border-red-400 border-b focus:border-red-500 ' : 'border-b border-rick-white focus:border-rick-green-1 transition-all',
+          { 'pr-10': type == 'password' }
+        ]" :placeholder="placeholder" :name="name" />
+      <Icon v-if="showPassword && type == 'password'" icon="mdi:eye-outline"
+        class="absolute right-2.5 text-rick-white cursor-pointer" @click="togglePassword" />
+      <Icon v-if="!showPassword && type == 'password'" icon="mdi:eye-off-outline"
+        class="absolute right-2.5 text-rick-white cursor-pointer" @click="togglePassword" />
     </div>
-    <span v-if="error" className='text-red-400 text-xs absolute'>{{error[0]}}</span>
+    <span v-if="error" className='text-red-400 text-xs absolute'>{{ error[0] }}</span>
   </div>
 </template>
 <script setup lang="ts">
